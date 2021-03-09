061a451cadd45e67b2ddf53b9d7440389fcd68ac
Stackbit Code Editor <support@stackbit.com>
2021-03-09 00:47:02 +0000

    Header.js: changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,7 +16,7 @@ export default class Header extends React.Component {
             				{_.get(this.props, 'pageContext.site.siteMetadata.header.logo', null) ? (
             				<Link className="navbar__logo m-0" to={withPrefix('/')}><img src={withPrefix(_.get(this.props, 'pageContext.site.siteMetadata.header.logo', null))} alt={_.get(this.props, 'pageContext.site.siteMetadata.header.logo_alt', null)} /></Link>
             				) : 
-            				<Link className="navbar__title h2 m-0 text-decoration-color: white
+            				<Link className="navbar__title h2 m-0 white
     background-color: rgb(14, 142, 226)" to={withPrefix('/')}>{_.get(this.props, 'pageContext.site.siteMetadata.header.title', null)}</Link>
             				}
             			</div>
